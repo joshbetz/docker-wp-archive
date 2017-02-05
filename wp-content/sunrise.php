156cebfefae559af88577682c0fe836bfba909d8
Josh Betz <j@joshbetz.com>
2017-02-05 15:12:04 +0100

    Remove custom mercator redirect plugin Add old domain mapping plugin for now

--- wp-content/sunrise.php
+++ wp-content/sunrise.php
@@ -2,5 +2,6 @@
 // Default mu-plugins directory if you haven't set it
 defined( 'WPMU_PLUGIN_DIR' ) or define( 'WPMU_PLUGIN_DIR', WP_CONTENT_DIR . '/mu-plugins' );
 
-require WPMU_PLUGIN_DIR . '/mercator/mercator.php';
-require WPMU_PLUGIN_DIR . '/mercator-sso-redirects/mercator-sso-redirects.php';
+//require WPMU_PLUGIN_DIR . '/mercator/mercator.php';
+require WPMU_PLUGIN_DIR . '/wordpress-mu-domain-mapping/sunrise.php';
+require WPMU_PLUGIN_DIR . '/wordpress-mu-domain-mapping/domain_mapping.php';
