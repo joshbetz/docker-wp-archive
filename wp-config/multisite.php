3338737763ac5c03431c082df437526738b64e21
Josh Betz <j@joshbetz.com>
2017-03-25 11:19:57 -0500

    Don't automatically install wordpress multisite

--- wp-config/multisite.php
+++ wp-config/multisite.php
@@ -1,8 +1,9 @@
 <?php
-if ( ! @constant('WP_INSTALLING') ) {
+
+define('WP_ALLOW_MULTISITE', true);
+
+if ( @constant('MULTISITE') && ! @constant('WP_INSTALLING') ) {
 	define('SUNRISE', true);
-	define('WP_ALLOW_MULTISITE', true);
-	define('MULTISITE', true);
 	define('SUBDOMAIN_INSTALL', true);
 	define('PATH_CURRENT_SITE', '/');
 	define('SITE_ID_CURRENT_SITE', 1);
